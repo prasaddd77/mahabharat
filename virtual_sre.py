601591f5934ebecdcda78c8bedc00011bd748720
Prasad Ranjane <99267538+prasaddd77@users.noreply.github.com>
2026-04-01 14:44:19 +0800

    Removing API key

--- virtual_sre.py
+++ virtual_sre.py
@@ -15,7 +15,7 @@ async def analyze_and_heal(log_file_path: str):
     #     print("ERROR: GEMINI_API_KEY environment variable not set.")
     #     sys.exit(1)
 
-    client = genai.Client(api_key="AIzaSyDoluDDmAg-MMF9-LWCUTvFLeHNU6GPUyM")
+    client = genai.Client(api_key="")
     
     # Path to the MCP server script
     server_script = os.path.join(os.path.dirname(os.path.abspath(__file__)), "mcp_server.py")
